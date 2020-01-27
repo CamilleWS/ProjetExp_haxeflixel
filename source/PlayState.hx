85f3895d997b837f75ee221631c91893ab56a374
camille.villa@epitech.eu <camille.villa@epitech.eu>
2020-01-27 15:25:07 +0100

    add a player moving

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -10,6 +10,10 @@ class PlayState extends FlxState
 		var text = new flixel.text.FlxText(0, 0, 0, "-- { Game here } --", 24);
 		text.screenCenter();
 		add(text);
+
+		var _player:Player;
+		_player = new Player(20, 20);
+		add(_player);
 	}
 
 	override public function update(elapsed:Float):Void
